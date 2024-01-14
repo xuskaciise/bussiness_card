009d562b2630f17c30ca84bd0a66c00636d2ce92
Eng Hussein <suldaanciisecali25@gmail.com>
2024-01-14 16:51:32 +0300

    todoapp

--- lib/main.dart
+++ lib/main.dart
@@ -1,79 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter/services.dart';
+import 'package:my_card/screens/Home.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: Home(),
-  ));
+  runApp(const MyApp());
 }
 
-class Home extends StatelessWidget {
-  const Home({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.cyan,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(
-            child: CircleAvatar(
-              radius: 55,
-              backgroundImage: AssetImage('images/profile.jpg'),
-            ),
-          ),
-          SizedBox(height: 12.0),
-          Text(
-            'Eng Hussein Isse',
-            style: GoogleFonts.pacifico(fontSize: 22.0),
-          ),
-          SizedBox(height: 8.0),
-          Text(
-            'Mobile Deloper',
-            style: TextStyle(letterSpacing: 3.0),
-          ),
-          SizedBox(
-            height: 12.0,
-          ),
-          Container(
-            height: 65,
-            margin: EdgeInsets.only(left: 22.0, right: 22.0),
-            padding: EdgeInsets.only(left: 12.0),
-            decoration: BoxDecoration(
-                color: Colors.white, borderRadius: BorderRadius.circular(12.0)),
-            child: Row(
-              children: [
-                Icon(
-                  Icons.phone,
-                  color: Colors.cyan,
-                ),
-                SizedBox(width: 12.0),
-                Text('+252615844908'),
-              ],
-            ),
-          ),
-          SizedBox(height: 12.0),
-          Container(
-            height: 65,
-            margin: EdgeInsets.only(left: 22.0, right: 22.0),
-            padding: EdgeInsets.only(left: 12.0),
-            decoration: BoxDecoration(
-                color: Colors.white, borderRadius: BorderRadius.circular(12.0)),
-            child: Row(
-              children: [
-                Icon(
-                  Icons.email,
-                  color: Colors.cyan,
-                ),
-                SizedBox(width: 12.0),
-                Text('xuseenyareciise@gmail.com'),
-              ],
-            ),
-          ),
-        ],
-      ),
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(statusBarColor: Colors.transparent),
+    );
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
     );
   }
 }
